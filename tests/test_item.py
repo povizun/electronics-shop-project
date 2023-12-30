e500a35f9d1378fdc97f34a8e3682f9e5d2521e5
Futeki <Povizun@bk.ru>
2023-12-30 06:49:30 +0300

    done second homework

--- tests/test_item.py
+++ tests/test_item.py
@@ -14,3 +14,25 @@ def test_apply_discount():
     Item.pay_rate = 0.9
     test_item.apply_discount()
     assert test_item.price == 80.91
+
+
+def test_name():
+    test_item.name = "Смартфон"
+    assert test_item.name == "Смартфон"
+    test_item.name = "СуперСмартфон"
+    assert test_item.name == "СуперСмарт"
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+    assert len(Item.all) == 5
+    assert Item.all[0].name == "Смартфон"
+    assert Item.all[4].name == "Клавиатура"
+    assert Item.all[0].price == 100
+    assert Item.all[0].quantity == 1
+
+
+def test_string_to_number():
+    assert Item.string_to_number("3") == 3
+    assert Item.string_to_number("3.2") == 3
+    assert Item.string_to_number("3.8") == 3
