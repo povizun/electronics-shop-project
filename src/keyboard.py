4ac2d040fedc47254cb2df73d7c7e56ba3219fbf
Futeki <Povizun@bk.ru>
2024-01-21 11:58:24 +0300

    done fifth homework

---
+++ src/keyboard.py
@@ -0,0 +1,22 @@
+from src.item import Item
+
+
+class MixinLang:
+    def __init__(self):
+        self.__language = "EN"
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == "EN":
+            self.__language = "RU"
+        else:
+            self.__language = "EN"
+
+
+class Keyboard(Item, MixinLang):
+    pass
+
+
